584110fdb125dc5600df777ae110eeed05bef88f
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2018-06-02 15:25:40 +0530

    Adding initial server setup code and gitignore file for the server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+/*require modules*/
+const express = require('express');
+
+/*initiate express*/
+const app = express();
+
+/*middlewares*/
+
+/*initiate server listening*/
+app.listen(3000,()=>{console.log('Server running on port 3000')});
